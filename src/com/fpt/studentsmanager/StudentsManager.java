dc499bdf4ea472e2a47477298135334cd9f077f5
Dong Vu <dongvdd00244@fpt.edu.vn>
2017-08-28 08:13:50 +0700

    complete code

--- src/com/fpt/studentsmanager/StudentsManager.java
+++ src/com/fpt/studentsmanager/StudentsManager.java
@@ -16,7 +16,7 @@ public class StudentsManager {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ArrayList<Students> student = new ArrayList<Students>();
         Students student01 = new Students("Nguyễn Văn A", "11/11/1111", "nva@fpt.vn");
         Students student02 = new Students("Tạ Văn B", "22/12/1222", "tvb@fpt.vn");
@@ -27,7 +27,7 @@ public class StudentsManager {
 
         Scanner scan = new Scanner(System.in);
         String choice;
-
+// print out menu
         for (;;) {
             System.out.println("\t Student Manager ");
             System.out.println("==============================");
@@ -40,6 +40,7 @@ public class StudentsManager {
             System.out.print("Your choice : ");
             choice = scan.nextLine();
             switch (choice) {
+                // print out all stuudent
                 case "1":
                     System.out.println(" ");
                     for (Students s : student) {
@@ -47,8 +48,9 @@ public class StudentsManager {
                     }
                     System.out.println("\nPress Enter to back the Menu");
                     scan.nextLine();
-                   
+
                     break;
+                // add stuudent
                 case "2":
                     System.out.print("How many student you wana add : ");
                     int addLength = scan.nextInt();
@@ -68,29 +70,48 @@ public class StudentsManager {
                     System.out.println("\nPress Enter to back the Menu");
                     scan.nextLine();
                     break;
+                // edit student info
                 case "3":
-                    System.out.println("choice 3");
+                    System.out.println("Find student by info (name, email) :");
+                    String mod = scan.nextLine();
+                    for (Students s : student) {
+                        int index = student.indexOf(s);
+                        if (mod.equalsIgnoreCase(s.getName()) || mod.equalsIgnoreCase(s.getEmail())) {
+                            System.out.println("Student found : " + s.getName() + "   " + s.getEmail() + "    " + s.getBirthday());
+                            System.out.println("Editing");
+                            System.out.print("Name : ");
+                            String name = scan.nextLine();
+                            System.out.print("Email : ");
+                            String email = scan.nextLine();
+                            System.out.println("Birthday : ");
+                            String birthday = scan.nextLine();
+                            student.get(index).setName(name);
+                            student.get(index).setEmail(email);
+                            student.get(index).setBirthday(birthday);
+                        }
+                    }
                     break;
+                // delete student by name or email
                 case "4":
                     System.out.println("Find student by info (name, email) :");
                     String del = scan.nextLine();
-                    for(Students s : student){
-                       int a =  student.indexOf(s);
-                        System.out.println(a);
-                      String foundName = s.getName();
-                      String foundEmail = s.getEmail();
-                      if(del.equalsIgnoreCase(foundName) || del.equalsIgnoreCase(foundEmail)){
-                          System.out.println("Student found : "+s.getName() + "   " + s.getEmail() + "    " + s.getBirthday());
-                          System.out.print("Press Y to DELETE , N to back the Menu : ");
-                          String delChoice = scan.nextLine();
-                          if(delChoice.equalsIgnoreCase("y")){
-                              
-                          } else if(delChoice.equalsIgnoreCase("n")){
-                              break;
-                          }
-                      }
+                    for (Students s : student) {
+                        int index = student.indexOf(s);
+                        if (del.equalsIgnoreCase(s.getName()) || del.equalsIgnoreCase(s.getEmail())) {
+                            System.out.println("Student found : " + s.getName() + "   " + s.getEmail() + "    " + s.getBirthday());
+                            System.out.print("Press Y to DELETE , N to back the Menu : ");
+                            String delChoice = scan.nextLine();
+                            if (delChoice.equalsIgnoreCase("y")) {
+                                student.remove(index);
+                                System.out.println("Success !!! \n");
+
+                            } else if (del.equalsIgnoreCase("n")) {
+                                break;
+                            }
+                        }
                     }
                     break;
+                // exit program
                 case "5":
                     System.exit(0);
                 default:
